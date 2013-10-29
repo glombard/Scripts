f12009446654e2cf1caf2172a5f5964805e2adb9
Gert Lombard <gert.lombard@gmail.com>
2013-10-29 11:56:53 +0000

    Added Get-7za and Get-cURL scripts.

---
+++ PowerShell-Installers/Get-7za.ps1
@@ -0,0 +1,27 @@
+﻿function Unzip-File
+{
+    param ([string]$zipFile, [string]$destFolder)
+    $shell = New-Object -Com Shell.Application
+    New-Item -ItemType Directory -Force -Path $destFolder
+    $zip = $shell.NameSpace($zipFile)
+    $shell.NameSpace($destFolder).CopyHere($zip.Items(), 16)
+}
+
+Write-Host "Installing 7-Zip Command Line (7za)..."
+$dest = "$env:ProgramFiles\7-Zip"
+$destFile = "$env:ProgramFiles\7-Zip\7za.exe"
+if (!(Test-Path $destFile))
+{
+    Write-Host "Downloading 7za..."
+    $wc = New-Object Net.WebClient
+    $url = 'http://downloads.sourceforge.net/sevenzip/7za920.zip'
+    $file = "$env:TEMP\7za920.zip"
+    $wc.DownloadFile($url,$file)
+    Write-Host "Unzipping to: $dest"
+    Unzip-File $file $dest
+    Remove-Item $file
+}
+else
+{
+    Write-Host "7za already installed."
+}
---
+++ PowerShell-Installers/Get-cURL.ps1
@@ -0,0 +1,30 @@
+﻿function Unzip-File
+{
+    param ([string]$zipFile, [string]$destFolder)
+    
+    $7z = "$env:ProgramFiles\7-Zip\7za.exe"
+    if (Test-Path $7z)
+    {
+        $info = New-Object Diagnostics.ProcessStartInfo
+        $info.FileName = $7z
+        $info.Arguments = "x -y -o""$destFolder"" ""$zipFile"""
+        $info.Verb = "runas"
+        $proc = [Diagnostics.Process]::Start($info)
+        $proc.WaitForExit()
+    }
+    else
+    {
+        $shell = New-Object -Com Shell.Application
+    	$zip = $shell.NameSpace($zipFile)
+        $shell.NameSpace($destFolder).CopyHere($zip.Items(), 16)
+    }
+}
+
+Write-Host "Installing cURL..."
+$wc = New-Object Net.WebClient
+$url = "http://www.paehl.com/open_source/?download=curl_732_0_ssl.zip"
+$file = "$env:TEMP\curl.zip"
+$wc.DownloadFile($url,$file)
+Write-Host "Unzipping to: $env:windir"
+Unzip-File $file $env:windir
+Remove-Item $file
