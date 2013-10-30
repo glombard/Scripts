0960dee67cd8a43cc98b906102c3c7bbea8fa138
Gert Lombard <gert.lombard@gmail.com>
2013-10-30 09:47:25 +0000

    Show hidden files

--- PowerShell-Installers/Set-Disable-HideFileExt.ps1
+++ PowerShell-Installers/Set-Disable-HideFileExt.ps1
@@ -1,1 +1,2 @@
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Type DWord -Value 0
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name Hidden -Type DWord -Value 1
