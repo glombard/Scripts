9d980deabaf8f584cf4365662de868851428e785
Gert Lombard <gert.lombard@gmail.com>
2013-11-14 10:51:55 +0000

    Updated Start-AzureRDP

--- Utils/Start-AzureRDP.ps1
+++ Utils/Start-AzureRDP.ps1
@@ -4,12 +4,13 @@ param (
 	[string]$name = $(Read-Host "Azure VM name")
 )
 
+$vm = Get-AzureVM -ServiceName $name
 # Start the Azure Virtual Machine first:
-if ((Get-AzureVM -ServiceName $name | select -expand PowerState) -ne 'Started') {
+if ($vm.PowerState -ne 'Started') {
 	Start-AzureVM -Name $name -ServiceName $name
 }
- 
+
 # Start Remote Desktop session:
-$port = get-AzureVM -ServiceName $name | Get-AzureEndpoint -Name 'Remote Desktop' | select -ExpandProperty Port
+$port = $vm | Get-AzureEndpoint -Name 'Remote Desktop' | select -ExpandProperty Port
 $server = "${name}.cloudapp.net:$port"
 Invoke-Expression "mstsc.exe /v:$server /f"
